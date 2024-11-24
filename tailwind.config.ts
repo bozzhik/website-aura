38b1fba211a62a51a90c09e31416a63a38a025ef
maxim <bozzhik@ya.ru>
2024-11-25 00:49:07 +0300

    add `Inter` font for extension

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,12 @@
 import {Config} from 'tailwindcss'
+import {fontFamily} from 'tailwindcss/defaultTheme'
 
 const config: Config = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Inter', ...fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],

de542cca89fcf3dec0d46fb3e57797f4fe236593
maxim <bozzhik@ya.ru>
2024-11-25 00:48:06 +0300

    add `tailwindcss` for styling
    https://ui.shadcn.com/docs/installation/vite

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import {Config} from 'tailwindcss'
+
+const config: Config = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+export default config

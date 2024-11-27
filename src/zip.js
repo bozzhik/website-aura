8e183ecd5550b9664f8a26144e103359ff775c26
maxim <bozzhik@ya.ru>
2024-11-27 19:52:54 +0300

    fix `zip` gulp script

--- src/zip.js
+++ src/zip.js
@@ -3,8 +3,9 @@ import zip from 'gulp-zip'
 import {createRequire} from 'module'
 const require = createRequire(import.meta.url)
 const manifest = require('../build/manifest.json')
+const packageData = require('../package.json')
 
 gulp
   .src('build/**')
-  .pipe(zip(`${manifest.name.replaceAll(' ', '-')}-${manifest.version}.zip`))
+  .pipe(zip(`${packageData.name}-${manifest.version}.zip`))
   .pipe(gulp.dest('package'))
